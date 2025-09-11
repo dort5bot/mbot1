9791483e80a8d62745020dd43159c56caa952d09
dort5bot <dersdep@gmail.com>
2025-09-11 13:46:46 +0300

    Delete handlers/report_handler.py

--- handlers/report_handler.py
+++
@@ -1,9 +0,0 @@
-from aiogram import Router, types
-from aiogram.filters import Command
-
-router = Router()
-
-@router.message(Command("j"))
-@router.message(Command("rapor"))
-async def cmd_report(message: types.Message):
-    await message.answer("📊 Rapor hazır!\n(Burada rapor içeriği olacak)")
